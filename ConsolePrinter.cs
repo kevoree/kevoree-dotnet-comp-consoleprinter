02246004788eedeeba1669b016cb7866db06b7b5
Manuel Leduc <manuel.leduc@inria.fr>
2015-10-14 10:57:01 +0200

    first commit

---
+++ ConsolePrinter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Org.Kevoree.Annotation;
+using Org.Kevoree.Core.Api;
+
+namespace Org.Kevoree.Library
+{
+    [ComponentType]
+    [Serializable]
+    public class ConsolePrinter : MarshalByRefObject, DeployUnit
+    {
+        [KevoreeInject]
+        private Context context;
+
+        [Input]
+        public void input(object msg)
+        {
+            Console.WriteLine(context.getInstanceName() + ">" + msg);
+        }
+    }
+}
